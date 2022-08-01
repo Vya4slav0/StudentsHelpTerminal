4e6a9cde936ea1cfcd252124bfe15ffe18a33147
Vya4slav0 <vya4slav4617@gmail.com>
2022-08-01 21:57:27 +0600

    Created structure of project

--- StudentsHelpTerminal/StudentsHelpTerminal/Views/Windows/MainWindow.xaml.cs
+++ StudentsHelpTerminal/StudentsHelpTerminal/Views/Windows/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace StudentsHelpTerminal
+namespace StudentsHelpTerminal.Views.Windows
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
