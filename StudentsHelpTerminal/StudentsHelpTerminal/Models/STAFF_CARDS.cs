c38fc6e838377045815aab5889b389e23da380bb
Vya4slav0 <vya4slav4617@gmail.com>
2022-09-09 13:35:26 +0600

    Moved edxm in "Models" folder

--- StudentsHelpTerminal/StudentsHelpTerminal/Models/STAFF_CARDS.cs
+++ StudentsHelpTerminal/StudentsHelpTerminal/Models/STAFF_CARDS.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace StudentsHelpTerminal
+namespace StudentsHelpTerminal.Models
 {
     using System;
     using System.Collections.Generic;
--- StudentsHelpTerminal/StudentsHelpTerminal/Views/Windows/MainWindow.xaml.cs
+++ StudentsHelpTerminal/StudentsHelpTerminal/Views/Windows/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace StudentsHelpTerminal.Views.Windows
 {
@@ -23,6 +10,7 @@ namespace StudentsHelpTerminal.Views.Windows
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ViewModels.MainWindowViewModel();
         }
     }
 }
