6add1449b3547f79f014894f0c28b64ec5081854
Vya4slav0 <vya4slav4617@gmail.com>
2023-05-13 17:35:10 +0600

    Added library to store dialog boxes, added confirmation dialog box

---
+++ StudentsHelpTerminal/DialogBoxes/ConfirmBox.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace DialogBoxes
+{
+    /// <summary>
+    /// Логика взаимодействия для ConfirmBox.xaml
+    /// </summary>
+    public partial class ConfirmBox : Window
+    {
+        public ConfirmBox()
+        {
+            InitializeComponent();
+            DialogResult = false;
+        }
+
+        public new bool ShowDialog()
+        {
+            return DialogResult.HasValue ? DialogResult.Value : false;
+        }
+
+        private void BtnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult= false;
+            Close();
+        }
+    }
+}
