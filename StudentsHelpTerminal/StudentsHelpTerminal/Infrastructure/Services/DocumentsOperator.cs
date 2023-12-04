fce294a31a5118a6465fd67f8936e9c61fd2c747
Vya4slav0 <vya4slav4617@gmail.com>
2023-12-04 12:00:30 +0600

    Added documents operator to get common and group documents lists

---
+++ StudentsHelpTerminal/StudentsHelpTerminal/Infrastructure/Services/DocumentsOperator.cs
@@ -0,0 +1,43 @@
+﻿using StudentsHelpTerminal.Models.Other;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StudentsHelpTerminal.Infrastructure.Services
+{
+    internal static class DocumentsOperator
+    {
+        //Searching for available documents
+        private static readonly string commonDocs = Properties.Settings.Default.CommonDocsDirectoryName;
+
+        public static IEnumerable<DocumentsListItem> GetGroupDocuments(string groupName)
+        {
+            string groupDocs = string.Format("{0}\\{1}", Properties.Settings.Default.PathToMainFolder, groupName);
+
+            //Create documents directory if not exist
+            if (!Directory.Exists(groupDocs)) Directory.CreateDirectory(groupDocs);
+
+            List<DocumentsListItem> result = new List<DocumentsListItem>();
+            //Searching for the group documents
+            foreach (string filePath in Directory.GetFiles(groupDocs))
+            {
+                DocumentsListItem file = new DocumentsListItem(filePath, DocumentsListItem.Type.Group);
+                if (file.FileInfo.Extension == ".xps")
+                    result.Add(file);
+            }
+            return result;
+        }
+
+        public static IEnumerable<DocumentsListItem> GetCommonDocuments()
+        {
+            List<DocumentsListItem> result = new List<DocumentsListItem>();
+            //Searching for documents for all students
+            foreach (string filePath in Directory.GetFiles(commonDocs))
+            {
+                DocumentsListItem file = new DocumentsListItem(filePath, DocumentsListItem.Type.Common);
+                if (file.FileInfo.Extension == ".xps")
+                    result.Add(file);
+            }
+            return result;
+        }
+    }
+}
--- StudentsHelpTerminal/StudentsHelpTerminal/ViewModels/MainPageViewModel.cs
+++ StudentsHelpTerminal/StudentsHelpTerminal/ViewModels/MainPageViewModel.cs
@@ -163,30 +163,9 @@ namespace StudentsHelpTerminal.ViewModels
             //Writing in log
             Logger.ClearLogIfFull();
             Logger.WriteLog(student);
-            
-            //Searching for available documents
-            string commonDocs = Properties.Settings.Default.CommonDocsDirectoryName;
-            string groupDocs = string.Format("{0}\\{1}",
-                Properties.Settings.Default.PathToMainFolder,
-                student.Group);
 
-            if (!Directory.Exists(groupDocs)) Directory.CreateDirectory(groupDocs);
-
-            //Searching for documents for all students
-            foreach (string filePath in Directory.GetFiles(commonDocs))
-            {
-                DocumentsListItem file = new DocumentsListItem(filePath, DocumentsListItem.Type.Common);
-                if (file.FileInfo.Extension == ".xps")
-                    AvailableDocs.Add(file);
-            }
-            
-            //Searching for this group documents
-            foreach(string filePath in Directory.GetFiles(groupDocs))
-            {
-                DocumentsListItem file = new DocumentsListItem(filePath, DocumentsListItem.Type.Group);
-                if (file.FileInfo.Extension == ".xps")
-                    AvailableDocs.Add(file);
-            }
+            AvailableDocs.AddRange(DocumentsOperator.GetCommonDocuments());
+            AvailableDocs.AddRange(DocumentsOperator.GetGroupDocuments(student.Group));
         }
     }
 }
