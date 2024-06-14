8dce80217ef7ad88f494adb97f02003f98d1bfac
Vya4slav0 <vya4slav4617@gmail.com>
2024-06-14 16:21:46 +0500

    Fixed LoadSettingByName

--- StudentsHelpTerminal/SettingsEditor/Infrastructure/Services/SettingsManagerService.cs
+++ StudentsHelpTerminal/SettingsEditor/Infrastructure/Services/SettingsManagerService.cs
@@ -28,21 +28,7 @@ namespace SettingsEditor.Infrastructure.Services
         /// <returns>SettingsEditor.Models.Setting</returns>s
         public Setting LoadSettingByName(string name)
         {
-            XElement root = _settingsXML.Root;
-            XElement[] allSections = root.Elements("section").ToArray();
-            foreach (XElement section in allSections)
-            {
-                XElement node = section.Elements("setting").First(s => s.Attribute("name").Value == name);
-                return new Setting
-                {
-                    Name = name,
-                    Type = Type.GetType(node.Element("type").Value),
-                    Value = node.Element("value").Value,
-                    Description = node.Element("description").Value,
-                    Section = section.Attribute("visibleName").Value
-                };
-            }
-            return null;
+            return LoadAllSettings().FirstOrDefault(x => x.Name == name);
         }
 
         /// <summary>
